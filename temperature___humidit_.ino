a922e8b92676c141dad8c8bcbba5bc85f7f30151
anass-jr <anass.robio5@gmail.com>
2021-08-21 00:29:23 +0100

    initial commit

---
+++ temperature___humidit_.ino
@@ -0,0 +1,25 @@
+#include <dht.h>
+
+
+dht DHT;
+
+#define DHT11_PIN 7
+
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop(){
+  
+  int chk = DHT.read11(DHT11_PIN);
+ 
+  Serial.print("Temp: ");
+  Serial.print(DHT.temperature);
+  Serial.print((char)223);
+  Serial.print("C");
+  
+ Serial.print("Humidite: ");
+ Serial.print(DHT.humidity);
+ Serial.print("%");
+  delay(1000);
+}
